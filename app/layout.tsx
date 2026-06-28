781583c10feec056961d3a02b99dc02a2aa310f8
sananasgarov <prog58816@gmail.com>
2026-06-29 01:37:33 +0400

    video f

--- app/layout.tsx
+++ app/layout.tsx
@@ -128,6 +128,7 @@ export default function RootLayout({
       className={`${geist.variable} ${antonio.variable} ${greatVibes.variable} h-full antialiased`}
     >
       <head>
+        <link rel="preload" as="video" href="/home1.mp4" type="video/mp4" />
         <script dangerouslySetInnerHTML={{ __html: `history.scrollRestoration = 'manual';` }} />
         <script
           type="application/ld+json"
